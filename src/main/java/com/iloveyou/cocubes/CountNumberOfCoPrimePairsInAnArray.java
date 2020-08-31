08c54bd868de1e69dab42d2c842c3d5def36f24a
avinashkumarprajapati@gmail.com <avinashkumarprajapati@gmail.com>
2020-08-31 10:36:06 +0530

    adding code

--- src/main/java/com/iloveyou/cocubes/CountNumberOfCoPrimePairsInAnArray.java
+++ src/main/java/com/iloveyou/cocubes/CountNumberOfCoPrimePairsInAnArray.java
@@ -1,8 +1,39 @@
 package com.iloveyou.cocubes;
 
+/**
+ * @author avinash
+ * @apiNote 1) Count the number of co-prime pairs in an array. (Any two numbers whose GCD is 1 are be called as co-prime)
+ * <br>
+ * <b> Input:</b><br>
+ * <b>
+ * The first line contains an integer T, total number of elements. Then follow T elements.
+ * </b><br>
+ * <b>
+ * Output:
+ * </b><br>
+ * ## Count the number of co-prime pairs in an array.
+ * Constraints:
+ * 1 ? T ? 25
+ * 1 ? elements ? 100
+ * Sample Input and Output:
+ * Input 1:
+ * 3
+ * 1 2 3
+ * Output 1:
+ * 3
+ * Here, Co-prime pairs are (1, 2), (2, 3), (1, 3)
+ * Input 2:
+ * 4
+ * 4 8 3 9
+ * Output 2:
+ * 4
+ * Here, Co-prime pairs are (4, 3), (8, 3), (4, 9 ), (8, 9 )
+ */
 public class CountNumberOfCoPrimePairsInAnArray {
 
     public static void main(String[] args) {
-        
+        for (int i = 0; i < 10; i++) {
+            System.out.println("CountNumberOfCoPrimePairsInAnArray.main");
+        }
     }
 }
